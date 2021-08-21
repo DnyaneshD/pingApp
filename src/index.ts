6164550c0668ef5484b35f706e95790e6242a398
Dnyanesh Deshpande <dnyan.desh@gmail.com>
2021-08-21 07:16:11 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const port = 8080; // default port to listen
 
 // define a route handler for the default home page
 app.get("/", (req, res) => {
-  res.send("Hello world this from test app!");
+  res.send("Hello from AWS!");
 });
 
 // start the Express server
